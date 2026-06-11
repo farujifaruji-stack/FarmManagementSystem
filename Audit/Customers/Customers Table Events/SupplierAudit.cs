e45c9ee14694f5e6062f3330a855a44c184f41f1
Walaa Faruja <farujifaruji@gmail.com>
2026-06-12 00:29:33 +0300

    Add files via upload

---
+++ Audit/Customers/Customers Table Events/SupplierAudit.cs
@@ -0,0 +1,21 @@
+﻿namespace FarmManagement.Models.Customers.Customers_Table_Events
+{
+    public class SupplierAudit
+    {
+
+        //Generic
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string Id { get; set; }
+
+        //All Audit
+        public AuditAction Actions { get; set; }
+        public string PropertyName { get; set; }
+        public string PreValue { get; set; }
+        public string NewValue { get; set; }
+        public string UserId { get; set; }
+        public string RecordId { get; set; } // The id of the row in the original table
+    }
+}
---
+++ Audit/Workers/Workers Tables Events/AttendanceSheetAudit.cs
@@ -0,0 +1,21 @@
+﻿namespace FarmManagement.Models.Workers.Workers_Tables_Events
+{
+    public class AttendanceSheetAudit
+    {
+
+        //Generic
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string Id { get; set; }
+
+        //All Audit
+        public AuditAction Actions { get; set; }
+        public string PropertyName { get; set; }
+        public string PreValue { get; set; }
+        public string NewValue { get; set; }
+        public string UserId { get; set; }
+        public string RecordId { get; set; } // The id of the row in the original table
+    }
+}
